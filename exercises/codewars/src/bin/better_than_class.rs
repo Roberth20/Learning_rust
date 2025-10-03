23dde395ea4bea03c84d77a8b59cb3360ff1de3a
Roberth <marcanoroberth98@gmail.com>
2025-10-03 16:40:22 -0300

    Better than average score

---
+++ exercises/codewars/src/bin/better_than_class.rs
@@ -0,0 +1,17 @@
+use std::u16;
+
+fn better_than_average(class_points: &[u16], your_points: u16) -> bool {
+    let s: u16 = class_points.iter().sum();
+    let l: u16 = class_points.len().try_into().unwrap();
+    your_points > s / l
+}
+
+// Improved
+fn better_than_average_v2(class_points: &[u16], your_points: u16) -> bool {
+    your_points > class_points.iter().sum::<u16>() / class_points.len() as u16
+}
+
+fn main() {
+    let points: [u16;3] = [4, 5, 6];
+    println!("{}", better_than_average(&points, 5));
+}
